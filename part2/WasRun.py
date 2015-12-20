0680d3ae5cc3a7663a62a467e781f51d8e590a2a
kyunooh <chm073@gmail.com>
2015-12-20 13:10:16 +0900

    finished 20 chapter

--- part2/WasRun.py
+++ part2/WasRun.py
@@ -9,6 +9,10 @@ class TestCase:
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
+
+    def tearDown(self):
+        pass
 
 
 class WasRun(TestCase):
@@ -18,25 +22,26 @@ class WasRun(TestCase):
 
     def setUp(self):
         self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
 
-    def testMethod(self):    
-        self.wasRun = 1
+    def testMethod(self):
+        self.WasRun = 1
+        self.log = self.log + "testMethod "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 
 class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
     
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
-
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
+        
 
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
 
