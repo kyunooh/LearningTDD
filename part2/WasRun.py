b6d486caac8556c7798772e0e3177d44141abec3
kyunooh <chm073@gmail.com>
2015-12-21 01:08:18 +0900

    finish chapter 21

--- part2/WasRun.py
+++ part2/WasRun.py
@@ -6,10 +6,13 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
 
     def tearDown(self):
         pass
@@ -31,17 +34,41 @@ class WasRun(TestCase):
     def tearDown(self):
         self.log = self.log + "tearDown "
 
+    def testBrokenMethod(self):
+        raise Exception
+
 
 class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
-    
+
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
+
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown " == test.log)
         
 
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
+
 
 TestCaseTest("testTemplateMethod").run()
-
+TestCaseTest("testResult").run()
+TestCaseTest("testFailedResult").run()
